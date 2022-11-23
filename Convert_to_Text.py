c282142aa843d03ed8594f64f8b2adc09007206a
tikhonova <tikhonova.t@pm.me>
2022-11-22 23:41:14 -0500

    Add script to convert MOBI and PDF to text

--- Convert_to_Text.py
+++ Convert_to_Text.py
@@ -2,26 +2,30 @@
 Convert PDF to text
 Convert mobi to text
 '''
+
 import os
 
 import mobi
+from tqdm import tqdm
 
 filepath = "E:/AlanWattsMaterialSorted/mobi/"
 dest_path = "E:/AlanWattsMaterialSorted/txt/"
 
-for root, dirs, files in os.walk(filepath):
-    for file in files:
-        filename = filepath + f'{file}'
-        # for duck in ducks:
-        #     duck = duck
-        print(filename)
-        tempdir, filepath = mobi.extract(filename)
-        file_content = open(filepath, "r")
-        content = file_content.read()
-        print(dest_path + f'{file}' + '.txt')
-        f = open(dest_path + f'{file}' + '.txt', "w")
-        f.write(content)
-        f.close()
-        break
-        
+for i in tqdm(range(100)):
+    for root, dirs, files in os.walk(filepath):
+        for file in files:
+            print(file)
+            filename = filepath + f'{file}'
+            print(filename)
+            # for duck in ducks:
+            #     duck = duck
+            tempdir, test = mobi.extract(filename)
+            print(tempdir, test)
+            file_content = open(test, "r", encoding="utf-8")
+            print(file_content.encoding)
+            content = file_content.read()
+            print(dest_path + f'{file}' + '.txt')
+            f = open(dest_path + f'{file}' + '.txt', "w", encoding="utf-8")
+            f.write(content)
+            f.close()
 # https://stackoverflow.com/questions/25665/python-module-for-converting-pdf-to-text
