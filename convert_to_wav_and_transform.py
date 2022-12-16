6ed4e38f0c0fe58cd464766add231f9607e3ee79
tikhonova <tikhonova.t@pm.me>
2022-12-15 23:49:12 -0500

    Add Celery to .wav converter

--- convert_to_wav_and_transform.py
+++ convert_to_wav_and_transform.py
@@ -31,27 +31,3 @@ for filename in os.listdir(filepath):
     if filename not in os.listdir(dest_path):
         convert_audio_to_wav.delay(filename, filepath,
                                    dest_path)  # .delay adds task to Celery queue allowing multiple audio files to be converted concurrently
-
-'''
-Convert mp4, mp3, avi to wav
-'''
-
-# import os
-#
-# from pydub import AudioSegment
-# from tqdm import tqdm
-#
-# # os.path.join('E:\\AlanWatts\\venv\\Lib\\site-packages\\ffmpeg-essentials_build\\bin')  # adding ffmpeg to Path
-#
-# filepath = "E:/AlanWattsMaterialSorted/mp3/"
-# dest_path = "E:/AlanWattsMaterialSorted/audio/"
-#
-# for filename in os.listdir(filepath):
-#     if filename not in os.listdir(dest_path):
-#         '''iterates through mp4 files'''
-#         for i in tqdm(range(100)):
-#             '''launches progress bar'''
-#             #   if filename[:-4] == "Why not now - 1.mp4_2022-11-19_19_52_45":  # testing
-#             #      print(f"{filepath}" + f"{filename}")
-#             given_audio = AudioSegment.from_file(f"{filepath}" + f"{filename}", format="mp3")
-#             given_audio.export(f"{dest_path}" + f"{filename[:-4]}.wav", format="wav")
